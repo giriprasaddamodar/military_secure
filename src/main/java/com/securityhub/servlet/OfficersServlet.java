82e6ae39e67821eda1d5501a568446e82d03dfe8
Giriprasaath D <139479258+giriprasaddamodar@users.noreply.github.com>
2025-12-22 15:03:03 +0530

    Implement OfficersServlet to redirect to officers.jsp

--- src/main/java/com/securityhub/servlet/OfficersServlet.java
+++ src/main/java/com/securityhub/servlet/OfficersServlet.java
@@ -1,1 +1,15 @@
-package com.securityhub.servlet; public class OfficersServlet { }
+package com.securityhub.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/officers")
+public class OfficersServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+        resp.sendRedirect("officers.jsp");
+    }
+}
