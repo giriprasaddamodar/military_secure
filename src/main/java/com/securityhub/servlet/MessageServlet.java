6e8029c82dc7ce609e3a3a08729dfbe025a4fba2
Giriprasaath D <139479258+giriprasaddamodar@users.noreply.github.com>
2025-12-22 15:03:21 +0530

    Add MessageServlet to handle message sending
    Implemented doPost method to handle message submissions.

--- src/main/java/com/securityhub/servlet/MessageServlet.java
+++ src/main/java/com/securityhub/servlet/MessageServlet.java
@@ -1,1 +1,22 @@
-package com.securityhub.servlet; public class MessageServlet { }
+package com.securityhub.servlet;
+
+import com.securityhub.dao.MessageDAO;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/sendMessage")
+public class MessageServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+        String message = req.getParameter("message");
+
+        MessageDAO dao = new MessageDAO();
+        dao.saveMessage("Admin", message);
+
+        resp.sendRedirect("messages.jsp");
+    }
+}
