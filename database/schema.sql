45c3b24e7eb0457e18297b436d63ec46c6260630
Giriprasaath D <139479258+giriprasaddamodar@users.noreply.github.com>
2025-12-22 14:54:35 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,20 @@
 CREATE DATABASE security_hub_db;
+USE security_hub_db;
+
+CREATE TABLE officers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    password VARCHAR(100),
+    role VARCHAR(20)
+);
+
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sender VARCHAR(100),
+    content TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO officers(name,email,password,role)
+VALUES ('Admin','admin@security.com','admin123','ADMIN');
